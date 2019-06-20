0dead74f321b5954e9f37f7a0709247274641c3e
kyuma-git <hokumon2@icloud.com>
2019-06-20 12:42:46 +0900

    update controller codes

--- app/controllers/comment_likes_controller.rb
+++ app/controllers/comment_likes_controller.rb
@@ -2,14 +2,14 @@
 
 class CommentLikesController < ApplicationController
   def create
-    @comment = Comment.find(params[:comment_id])
-    @comment.add_like(current_user)
-    redirect_to @comment.post
+    comment = Comment.find(params[:comment_id])
+    comment.add_like(current_user)
+    redirect_to comment.post
   end
 
   def destroy
-    @comment = Comment.find(params[:comment_id])
-    @comment.delete_like(current_user)
-    redirect_to @comment.post
+    comment = Comment.find(params[:comment_id])
+    comment.delete_like(current_user)
+    redirect_to comment.post
   end
 end
