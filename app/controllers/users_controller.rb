466a7242314f45d8a58f68cb6e1d49624ad7609b
kyuma-git <hokumon2@icloud.com>
2019-07-13 18:45:58 +0900

    enable to follow from browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def index
+    @users = User.where.not(id: current_user.id).includes(:passive_relationships)
+  end
+
   def show
     @user = User.find(params[:id])
     # このユーザのポスト一覧（余裕があったら作る）
